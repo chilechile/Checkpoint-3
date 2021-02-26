d052b5d724ad8b796c8b648fab7932fe434e808a
chilechile <amontgomery313@noscihigh.org>
2021-02-26 17:43:54 +0000

    Finished work for this assignment.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 /*
 Declare a variable named me and 
 assign it to an object.
-
+var me = "car";
 And, assign me a key of firstName 
 and set it equal to your first name.
-
+var meFirstName = "alexis"
 Then, set another key of lastName 
 and set it equal to your last name
-*/
+*/ var meLastName = "Montgomery"; 
 
 
 
@@ -19,14 +19,14 @@ and set it equal to your last name
 /*
 Create a hobbies array and fill the 
 array with string values of your favorite hobbies. 
-
+var myHobbies = "sleeping"; 
 Add your hobbies array to your me object with 
 the key of myHobbies. (you must use dot 
 notation to add the above property). 
-
+var meHobbies = "sleeping" : "playing" : "tv"; 
 Then, add a pet key to your me object, and assign 
 it a value of an object with 2 keys of name and 
 species that are set to the name of your pet and 
 its species. (if you don't have a pet, you can just 
-make one up)
-*/
+make one up) 
+*/ var mePet = ("dog", "goldie");
